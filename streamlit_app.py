6d760509b18bf8d24591ad381439158c64f283e9
Shawn Ravi <91684800+Shawn191@users.noreply.github.com>
2022-05-29 22:06:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
-from PIL import Image
 
 st.set_page_config(page_title='Plastic Pollution Data Visualisations')
 st.title("Earthlly")
